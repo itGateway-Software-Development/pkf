b3606ccc07400d81075a60e9680cda44d50c8850
minnoaksoelat-ui <minnoaksoelat@gmail.com>
2026-06-13 01:20:19 +0700

    major changes to index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,60 @@
-import { createStore } from 'vuex'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
-export default createStore({
-  state: {
-    currentRoute: '',
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
   },
-  getters: {
-    getCurrentRoute : (state) => state.currentRoute
+  {
+    path: '/about-us',
+    name: 'AboutUs',
+    component: () => import('../views/AboutUs.vue')
   },
-  mutations: {
-    setCurrentRoute : (state, route) => {
-      state.currentRoute = route;
-    }
+  {
+    path: '/services/:service',
+    name: 'Services',
+    component: () => import('../views/Services.vue'),
+    props: true
   },
-  actions: {
-    getRoute : (context, route) => {
-      context.commit('setCurrentRoute', route)
-    }
-
+  {
+    path: '/news-events',
+    name: 'NewsEvents',
+    component: () => import('../views/NewsEvents.vue')
   },
-  modules: {
+  {
+    path: '/careers/:career',
+    name: 'Careers',
+    component: () => import('../views/Careers.vue'),
+    props: true,
+  },
+  {
+    path: '/careers/:career/apply',
+    name: 'ApplyJob',
+    component: () => import('../views/ApplyJob.vue'),
+    props: true,
+  },
+  {
+    path: '/news-events/:event',
+    name: 'EventDetail',
+    component: () => import('../views/EventDetail.vue')
+  },
+  {
+    path: '/contact-us',
+    name: 'ContactUs',
+    component: () => import('../views/ContactUs.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('../views/404Page.vue')
   }
+ 
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
+
+export default router
